d64d7342a60f515cbb78323c0d5e341a47217010
Josue-Niyonkunda <josueniyonkunda11@gmail.com>
2026-04-24 16:47:24 +0200

    msg

--- src/test/java/com/test/specBuilder/TestJenkins.java
+++ src/test/java/com/test/specBuilder/TestJenkins.java
@@ -3,6 +3,6 @@ package com.test.specBuilder;
 public class TestJenkins {
     public static void entre(){
         System.out.println("you are the second");
-        System.out.println("Good");
+
     }
 }
